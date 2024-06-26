ee0d65877d9029775dbb6d2ed27a9ec6bacb4421
Zhi Qiang QUEK <zhiqiang.quek@biposervice.com>
2024-06-26 14:00:17 +0800

    Shifted loading of processor and model to utils from imagecaption

--- Transformer_BLIP/ImageCaptioning.py
+++ Transformer_BLIP/ImageCaptioning.py
@@ -1,24 +1,8 @@
 import os
 import PIL
-from typing import Tuple
 from PIL import Image
-from transformers import BlipProcessor, BlipForConditionalGeneration
+from utils import load_blip_processor_and_model
 from dotenv import load_dotenv
-
-def load_blip_processor_and_model(blip_model_name: str = "Salesforce/blip-image-captioning-large") -> Tuple[BlipProcessor,BlipForConditionalGeneration]:
-    """Function which loads and returns Blip model Processor and Models.
-
-    Args:
-        blip_model_name (str, optional): Blip model name to use. Defaults to "Salesforce/blip-image-captioning-large" model.
-
-    Returns:
-        Tuple[BlipProcessor,BlipForConditionalGeneration]: Pretrained BlipProcessor and BlipForConditionalGeneration classes.
-    """
-    processor = BlipProcessor.from_pretrained(blip_model_name)
-    
-    model = BlipForConditionalGeneration.from_pretrained(blip_model_name)
-
-    return processor, model
 
 
 def open_image_in_rgb(img_filepath: str) -> Image:
