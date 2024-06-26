1ef694470eab10d4e37dfaec2195cc289974f7c7
Zhi Qiang QUEK <zhiqiang.quek@biposervice.com>
2024-06-26 17:49:33 +0800

    Added simple chatbot application interfaced by terminal with loop mechanism

--- Chatbot/chatbot.py
+++ Chatbot/chatbot.py
@@ -1,7 +1,10 @@
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 from dotenv import load_dotenv
+from utils import load_model_tokenizer_and_clean_convo_hist
 import os
 
+"""
+Script which enables chatbot-human interaction via terminal/command prompt application powered by a Huggingface's Chatbot Model with conversation history stored on the fly.
+"""
 
 if __name__ == "__main__":
     load_dotenv()
@@ -10,31 +13,26 @@ if __name__ == "__main__":
     # Load model (download on first run and reference local installation for consequent runs)
     hface_auth_token = os.environ.get("HUGGINGFACEHUB_API_TOKEN")
 
-    # Define model and tokenizer
-    model = AutoModelForSeq2SeqLM.from_pretrained(
-        pretrained_model_name_or_path = "facebook/blenderbot-400M-distill", token = hface_auth_token
-    )
-    tokenizer = AutoTokenizer.from_pretrained(
-        pretrained_model_name_or_path="facebook/blenderbot-400M-distill", token=hface_auth_token
+    # Load model,tokenizer and convo history
+    model, tokenizer, conversation_history = load_model_tokenizer_and_clean_convo_hist(
+        model_name=model_name, token=hface_auth_token
     )
 
-    #List to store conversation history
-    conversation_history = []
-    
     while True:
         history_string = "\n".join(conversation_history)
 
+        # Seek user input prompt
         input_text = input("> ")
         # Encode history to tokens
         inputs = tokenizer.encode_plus(history_string, input_text, return_tensors="pt")
 
         
         # Gnerate output and decode
-        outputs = model.generate(**inputs)
+        outputs = model.generate(**inputs, max_length=os.environ.get("MODEL_MAX_TOKEN"))
         response = tokenizer.decode(outputs[0], skip_special_tokens=True).strip()
 
         # Append to conversation history
         conversation_history.append(input_text)
         conversation_history.append(response)
-        #print(conversation_history)
-        #print(response)
+        print(conversation_history)
+        print(response)
