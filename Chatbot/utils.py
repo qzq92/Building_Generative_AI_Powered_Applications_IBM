9a13cd4f2b14d0322c556c4b62e3d6bfc256fee1
Zhi Qiang QUEK <zhiqiang.quek@biposervice.com>
2024-06-27 10:30:20 +0800

    Added docstring and typings as output

--- Chatbot/utils.py
+++ Chatbot/utils.py
@@ -1,13 +1,23 @@
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+from typing import Tuple
 
-def load_model_tokenizer_and_clean_convo_hist(model_name:str, token:str):
+def load_model_tokenizer_and_clean_convo_hist(model_name:str, token:str) -> Tuple(AutoModelForSeq2SeqLM, AutoTokenizer, list):
+    """Function which loads transformers library AutoModelForSeq2SeqLM, AutoTokenizer based on model and input token and returns them together with an empty list of conversation history.
+
+    Args:
+        model_name (str): Valid model name found in HuggingFaceHub.
+        token (str): HuggingFaceHub API token.
+
+    Returns:
+        Tuple: Containing AutoModelForSeq2SeqLM, AutoTokenizer, list
+    """
     model = AutoModelForSeq2SeqLM.from_pretrained(
-        model_name,
-        token=token
+        pretrained_model_name_or_path = model_name,
+        token = token
     )
     tokenizer = AutoTokenizer.from_pretrained(
-        model_name,
-        token=token
+        pretrained_model_name_or_path = model_name,
+        token = token
     )
     conversation_history = []
 
