9a98a95bb43940080e4af7854d8a3aba7120f54f
Zhi Qiang QUEK <zhiqiang.quek@biposervice.com>
2024-06-28 16:06:56 +0800

    Streamline 2 text-to-speech function as 1

--- VoiceAssistant/worker.py
+++ VoiceAssistant/worker.py
@@ -18,7 +18,7 @@ def speech_to_text(audio_binary: list) -> str:
         audio_binary (str): Array of values sampled from audio.
 
     Returns:
-        str: _description_
+        str: Transcribed speech in text.
     """
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
     torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
@@ -30,7 +30,7 @@ def speech_to_text(audio_binary: list) -> str:
     
     if stt_model.startswith("openai/whisper"):
         model = WhisperForConditionalGeneration.from_pretrained(
-            pretrained_model_name_or_path=stt_model 
+            pretrained_model_name_or_path=stt_model
         )
         processor = WhisperProcessor.from_pretrained(
             pretrained_model_name_or_path=stt_model
@@ -39,14 +39,22 @@ def speech_to_text(audio_binary: list) -> str:
             audio_binary, sampling_rate=44100, return_tensors="pt"
         ).input_features
         
-        # Generate token ids and decode them (returns a list)
-        predicted_ids = model.generate(input_features)
+        # Generate token ids and decode them (returns a list). Configuration would be impactful for long form transcription
+        predicted_ids = model.generate(
+            input_features=input_features,
+            language="en",
+            task="transcribe",
+            do_sample=True,
+            temperature=float(os.environ.get("MODEL_STT_TEMPERATURE"))
+        )
         transcription = processor.batch_decode(
-            predicted_ids, skip_special_tokens=True
+            predicted_ids,
+            skip_special_tokens=True,
         )
         print(transcription)
 
         return transcription[0]
+    
     # Assume other models that is not from OpenAI
     model = AutoModelForSpeechSeq2Seq.from_pretrained(
         pretrained_model_name_or_path=stt_model,
@@ -66,51 +74,47 @@ def speech_to_text(audio_binary: list) -> str:
         tokenizer=processor.tokenizer,
         feature_extractor=processor.feature_extractor,
         torch_dtype=torch_dtype,
-        max_new_tokens=128,
+        max_new_tokens=int(os.environ.get("MODEL_STT_MAX_TOKEN")),
         device=device,
+        do_sample=True,
+        temperature=float(os.environ.get("MODEL_STT_TEMPERATURE"))
     )
 
     result = pipe(audio_binary)
     return result["text"]
 
-def text_to_speech_infer_endpoints(input_text: str) -> Any:
+def text_to_speech(input_text: str, api_call: bool = True) -> Any:
     """Function which calls TTS model as a service through inference endpoint API to perform text to speech generation.
 
     Args:
         input_text (str): Input text to be synthesized.
+        api_call (bool): Boolean state to control whether to do inference via API call directly.
     """
+    if api_call:
         # Set the headers for our HTTP request
-    headers = {
-        "Authorization": f"Bearer {os.environ.get("HUGGINGFACEHUB_API_TOKEN")}"
-    }
+        headers = {
+            "Authorization": f"Bearer {os.environ.get("HUGGINGFACEHUB_API_TOKEN")}"
+        }
 
-    model_name = os.environ.get("HUGGINGFACE_TTS_MODEL_NAME")
-    inference_base_api = "https://api-inference.huggingface.co/models/"
-    api_end_point = inference_base_api + model_name
+        model_name = os.environ.get("HUGGINGFACE_TTS_MODEL_NAME")
+        inference_base_api = "https://api-inference.huggingface.co/models/"
+        api_end_point = inference_base_api + model_name
 
-    # Set the body of our HTTP request
-    request_payload = {
-        "text_inputs": input_text,
-    }
+        # Set the body of our HTTP request
+        request_payload = {
+            "text_inputs": input_text,
+        }
 
-    response = requests.post(
-        url=api_end_point,
-        headers=headers,
-        json=request_payload,
-        timeout=15
-    )
-    print('text to speech response:', response)
-    return response.content
+        response = requests.post(
+            url=api_end_point,
+            headers=headers,
+            json=request_payload,
+            timeout=15
+        )
+        print('text to speech response:', response)
+        return response.content
 
-def text_to_speech_direct_inference(input_text: str) -> Any:
-    """Function which synthesizes input text into audio using HuggingFace's transformer pipeline for direct inferencing.
-
-    Args:
-        input_text (str): Input text to be synthesized
-
-    Returns:
-        Any: Synthesized Speech object.
-    """
+    # If not api call
     synthesizer = pipeline(
         task="text-to-speech",
         model=os.environ.get("HUGGINGFACE_TTS_MODEL_NAME")
